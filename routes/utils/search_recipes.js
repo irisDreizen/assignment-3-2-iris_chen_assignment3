c6c6201746a823ac310a75b55e630d3b5d2f0881
irisDreizen <irisdrei7@gmail.com>
2020-07-08 20:18:13 +0300

    Update search_recipes.js

--- routes/utils/search_recipes.js
+++ routes/utils/search_recipes.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const api_url = "https://api.spoonacular.com/recipes";
-const api_key = "apiKey=370edf545a26487881977b6a1aeaa8e8";
+const api_key = "apiKey=0a9deb3880cd49c8bcd8d5a613a8f3d8";
 
 // *********search for recipe*************
 function extractQureryParams(query_params, search_params){
